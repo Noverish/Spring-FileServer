45cd188a6e09288c97ded1eaf0685f4063ebdca2
Noverish <embrapers263@gmail.com>
2021-01-12 17:09:34 +0900

    Range Video

--- src/main/java/kim/hyunsub/file/MainController.java
+++ src/main/java/kim/hyunsub/file/MainController.java
@@ -2,15 +2,16 @@ package kim.hyunsub.file;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URLDecoder;
@@ -27,20 +28,19 @@ public class MainController {
     private ImageService imageService;
 
     @GetMapping("/**/*.{ext:(?:jpg|jpeg|png|JPG|JPEG|PNG)}")
-    public void jpg(HttpServletRequest request,
-                    HttpServletResponse response,
-                    @PathVariable String ext,
-                    @RequestParam(required=false, name="w") Integer width,
-                    @RequestParam(required=false, name="h") Integer height) throws IOException {
+    public void image(HttpServletRequest request,
+                      HttpServletResponse response,
+                      @PathVariable String ext,
+                      @RequestParam(required=false, name="w") Integer width,
+                      @RequestParam(required=false, name="h") Integer height) throws IOException {
         String pathStr = URLDecoder.decode(request.getRequestURI(), "UTF-8");
         Path path = Paths.get(ROOT_PATH, pathStr);
-        File file = path.toFile();
 
         if (!Files.isRegularFile(path)) {
             throw new NotFileException();
         }
 
-        BufferedImage input = ImageIO.read(new FileInputStream(file));
+        BufferedImage input = ImageIO.read(new FileInputStream(path.toFile()));
         BufferedImage output;
 
         if (width != null && height != null) {
@@ -55,6 +55,21 @@ public class MainController {
 
         response.setContentType(Files.probeContentType(path));
         response.getOutputStream().write(imageService.imageToByteArr(output, ext));
+    }
+
+    @GetMapping("/**/*.{ext:(?:mp4|MP4)}")
+    public ResponseEntity<FileSystemResource> video(HttpServletRequest request) throws IOException {
+        String pathStr = URLDecoder.decode(request.getRequestURI(), "UTF-8");
+        Path path = Paths.get(ROOT_PATH, pathStr);
+
+        if (!Files.isRegularFile(path)) {
+            throw new NotFileException();
+        }
+
+        return ResponseEntity
+                .ok()
+                .contentType(new MediaType("video", "mp4"))
+                .body(new FileSystemResource(path.toFile()));
     }
 
     @GetMapping("/**")
